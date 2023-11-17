1b4a144dc60a510f205942302a33fed84b091b2b
Het Patel <hetpatel07396@gmail.com>
2023-11-17 15:37:49 +0530

    updated sresult function

--- Lab 10/funtion_definitions.cpp
+++ Lab 10/funtion_definitions.cpp
@@ -72,7 +72,6 @@ void status_enqueue(int sockfd,int reqID)
 		s_rear->next = node;
 		s_rear = node;
 	}
-
 }
 
 struct statusq* status_dequeue()  
@@ -84,20 +83,17 @@ struct statusq* status_dequeue()
     while(s_front == NULL)
     {  
 		pthread_cond_wait(&status_cond,&status_queue_mutex);
-          
     }  
 	
     temp = s_front;  
 	if(s_front==s_rear)  
     {  
         s_front=s_rear=NULL;  
-        
     }  
     else  
     {  
         s_front=s_front->next;  
         s_rear->next=s_front;  
-         
     }  
    
 	pthread_mutex_unlock(&status_queue_mutex);
@@ -131,69 +127,66 @@ struct processq* process_dequeue()
     while(p_front == NULL)
     {  
 		pthread_cond_wait(&process_cond,&process_queue_mutex);
-       
     }  
-    
     temp = p_front;  
 	if(p_front==p_rear)  
     {  
         p_front=p_rear=NULL;  
-       
     }  
     else  
     {  
         p_front=p_front->next;  
-        p_rear->next=p_front;  
-        
+        p_rear->next=p_front;   
     }  
-   
-
 	pthread_mutex_unlock(&process_queue_mutex);
 	return temp;
 }
 
-void sresult(int newsockfd,int fp,int a,char * buffer)
+void sresult(int newsockfd, int fp, int a, char *buffer) 
 {
-	bzero(buffer,BUFFER_SIZE);
-	int x,n=0;
-	if(fp==-1)
-	{
-		n = send(newsockfd,"PASS\n",strlen("PASS\n"),0);
-		if(n < 0)
-			error("Send failed");
-		return;
-	}
-	else if(a==0)
-	{
-		strcat(buffer,"\nCOMPILE ERROR\n\n");
-		x = strlen("\nCOMPILE ERROR\n\n");
-	}
-	else if(a==1)
-	{
-		strcat(buffer,"\nRUNTIME ERROR\n\n");
-		x = strlen("\nRUNTIME ERROR\n\n");
-	}
+    memset(buffer, 0, BUFFER_SIZE);
+    int x, n = 0;
+    
+    if(a == 0)
+    {
+        strcat(buffer, "\nCOMPILE ERROR\n\n");
+        x = strlen("\nCOMPILE ERROR\n\n");
+    } 
+    else if(a == 1) 
+    {
+        n = send(newsockfd, "\nRUNTIME ERROR\n\n", strlen("\nRUNTIME ERROR\n\n"), 0);
+        if (n < 0)
+            error("Send failed");
+        return;
+    } 
+    else if(a == 2)
+    {
+        strcat(buffer, "\nOUTPUT ERROR \n\n");
+        x = strlen("\nOUTPUT ERROR \n\n");
+    }
 	else
-	{
-		strcat(buffer,"\nOUTPUT ERROR \n\n");
-		x = strlen("\nOUTPUT ERROR \n\n");
-	}
-	int k,flag=0;
-	while(1)
-	{
-		if(flag)
-			x = 0;
-		k = read(fp,buffer + x,BUFFER_SIZE-x);
+    {
+        n = send(newsockfd, "PASS\n", strlen("PASS\n"), 0);
+        if (n < 0)
+            error("Send failed");
+        return;
+    } 
+    int k, flag = 0;
+    while (true) 
+    {
+        if (flag)
+            x = 0;
+        k = read(fp, buffer + x, BUFFER_SIZE - x);
 
-		flag=1;
-		n = send(newsockfd,buffer,k+x,0);
-		if(n < 0)
-			error("Send failed");
-		if(k <= 0)
-			break;
-		
-		bzero(buffer,BUFFER_SIZE);
-	}
+        flag = 1;
+        n = send(newsockfd, buffer, k + x, 0);
+        if (n < 0)
+            error("Send failed");
+        if (k <= 0)
+            break;
+
+        memset(buffer, 0, BUFFER_SIZE);
+    }
 }
 
 int generateUniqueID()
